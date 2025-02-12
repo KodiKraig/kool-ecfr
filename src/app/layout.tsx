7e6d76ef2ab06e9d9edaa7b525ac05d4f2350bc5
KodiKraig <cody.craig@aol.com>
2025-02-12 16:57:23 -0500

    Move main to layout for reuse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+            {children}
+          </main>
+        </TRPCReactProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,15 @@ const Agencies = async () => {
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
-        <div className="flex flex-col items-center gap-2">
-          <h1 className="text-3xl font-bold">Code of Federal Regulations</h1>
-          <h2 className="text-gray-200">Select an agency to view metrics</h2>
-        </div>
-
-        <Suspense fallback={<div>Loading agencies...</div>}>
-          <Agencies />
-        </Suspense>
+    <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-3xl font-bold">Code of Federal Regulations</h1>
+        <h2 className="text-gray-200">Select an agency to view metrics</h2>
       </div>
-    </main>
+
+      <Suspense fallback={<div>Loading agencies...</div>}>
+        <Agencies />
+      </Suspense>
+    </div>
   );
 }
