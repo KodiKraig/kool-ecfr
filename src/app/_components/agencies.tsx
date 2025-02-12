819980610cf7383d8acf6321e6e201a5dc5203e8
KodiKraig <cody.craig@aol.com>
2025-02-12 16:02:50 -0500

    Remove pointless prefetching

--- src/app/_components/agencies.tsx
+++ src/app/_components/agencies.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { api } from "@/trpc/react";
 import { type Agency } from "@/server/lib/ecfr/admin";
 import { ArrowTurnDownRight } from "@/app/_icons/arrow-turn-down-right";
 import { useState } from "react";
@@ -8,7 +7,7 @@ import { useState } from "react";
 const Agency = ({ agency }: { agency: Agency }) => {
   return (
     <div>
-      <button className="flex w-full items-center justify-between text-wrap rounded-lg p-2 font-semibold transition-all hover:bg-gray-700 hover:text-gray-200">
+      <button className="flex w-full items-center justify-between text-wrap rounded-lg p-2 text-start font-semibold transition-all hover:bg-gray-700 hover:text-gray-200">
         <p>{agency.name}</p>
         <p className="pr-2 text-sm text-gray-300">
           {agency.cfr_references.length}
@@ -30,7 +29,7 @@ const Agency = ({ agency }: { agency: Agency }) => {
   );
 };
 
-const AgencyList = ({ agencies }: { agencies: Agency[] }) => {
+export const AgencyList = ({ agencies }: { agencies: Agency[] }) => {
   const [filteredAgencies, setFilteredAgencies] = useState(agencies);
   const [filter, setFilter] = useState("");
 
@@ -77,21 +76,17 @@ const AgencyList = ({ agencies }: { agencies: Agency[] }) => {
 
   return (
     <div className="flex w-3/4 flex-col items-center md:w-1/2">
-      <div className="flex w-full flex-col items-center pb-4">
-        <h1 className="text-3xl font-bold">Code of Federal Regulations</h1>
-
-        <p className="text-sm text-gray-400">{totalCount} agencies found</p>
-      </div>
-
       <input
         type="text"
         value={filter}
         onChange={(e) => handleFilterTextChange(e.target.value)}
-        placeholder="Select an agency"
-        className="mb-2 w-full rounded-md bg-gray-600 p-2 focus:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700"
+        placeholder="Search for an agency"
+        className="w-full rounded-md bg-gray-600 p-2 focus:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700"
       />
 
-      <div className="flex max-h-[80vh] w-full flex-col overflow-y-auto rounded-b-md border-t border-gray-600 pt-2">
+      <p className="py-2 text-sm text-gray-400">{totalCount} agencies found</p>
+
+      <div className="flex max-h-[60vh] w-full flex-col overflow-y-auto border-b border-t border-gray-600 py-2">
         {filteredAgencies.length === 0 && (
           <div className="p-2">No agencies found</div>
         )}
@@ -102,15 +97,4 @@ const AgencyList = ({ agencies }: { agencies: Agency[] }) => {
       </div>
     </div>
   );
-};
-
-export const Agencies = () => {
-  const [{ agencies }, { isLoading, error }] =
-    api.admin.agencies.useSuspenseQuery();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (error) return <div>Error: {error.message}</div>;
-
-  return <AgencyList agencies={agencies} />;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
-import { Agencies } from "@/app/_components/agencies";
-import { api, HydrateClient } from "@/trpc/server";
+import { AgencyList } from "@/app/_components/agencies";
+import { api } from "@/trpc/server";
+import { Suspense } from "react";
+
+const Agencies = async () => {
+  const { agencies } = await api.admin.agencies();
+
+  return <AgencyList agencies={agencies} />;
+};
 
 export default async function Home() {
-  void api.admin.agencies.prefetch();
-
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <Agencies />
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-3xl font-bold">Code of Federal Regulations</h1>
+          <h2 className="text-gray-200">Select an agency to view metrics</h2>
         </div>
-      </main>
-    </HydrateClient>
+
+        <Suspense fallback={<div>Loading agencies...</div>}>
+          <Agencies />
+        </Suspense>
+      </div>
+    </main>
   );
 }
