25df60e5ead0e948d34f230f4bc1bddbb8bd88a0
KodiKraig <cody.craig@aol.com>
2025-02-13 16:42:32 -0500

    Change color of card to match theme

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,6 +1,6 @@
 export const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center gap-2 rounded-lg bg-gray-900 p-4">
+    <div className="bg-darkBlue flex flex-col items-center gap-2 rounded-lg p-4">
       {children}
     </div>
   );
